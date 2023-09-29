a53545eee3225bdd3cbf517444ec3d544201f0ef
magha <dev.magha@gmail.com>
2023-09-29 14:19:07 +0300

    change composer.json

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
+use slg\SlugGenerator\Http\Controllers\DashboardController;
 
-Route::group( function () {
+Route::group( [], function () {
     Route::get('/dashboard', DashboardController::class, 'index')->name('index');
     Route::get('/slug-history', DashboardController::class, 'slugHistory')->name('slug-history');
     Route::get('/slug-generate', DashboardController::class, 'generateSlug')->name('generate-slug');
